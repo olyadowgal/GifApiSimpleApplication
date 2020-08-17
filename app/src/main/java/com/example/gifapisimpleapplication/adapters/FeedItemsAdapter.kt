4fd40f5626044869ba147e48e599e7706e2fa2c4
olyadowgal <dowgalolya@gmail.com>
2020-08-17 19:49:12 +0300

    Change favorites to a checkbox

--- app/src/main/java/com/example/gifapisimpleapplication/adapters/FeedItemsAdapter.kt
+++ app/src/main/java/com/example/gifapisimpleapplication/adapters/FeedItemsAdapter.kt
@@ -11,9 +11,10 @@ import com.example.gifapisimpleapplication.entities.GifInfo
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_feed.view.*
 
-class FeedItemsAdapter(val callback : Callback) : PagedListAdapter<GifInfo, FeedItemsAdapter.FeedItemViewHolder>(
-    GifInfo.DIFF_CALLBACK
-) {
+class FeedItemsAdapter(val callback: Callback) :
+    PagedListAdapter<GifInfo, FeedItemsAdapter.FeedItemViewHolder>(
+        GifInfo.DIFF_CALLBACK
+    ) {
 
     interface Callback {
 
@@ -42,25 +43,28 @@ class FeedItemsAdapter(val callback : Callback) : PagedListAdapter<GifInfo, Feed
         }
 
         fun onBind(item: GifInfo?) {
-            containerView.txt_gif_name.text =
-                item?.title ?: containerView.context.getString(R.string.id_as_name, item?.id)
             if (item == null) {
                 //Maybe load placeholder image
             } else {
-                Glide.with(containerView.context).load(item.url).into(containerView.img_gif_preview)
-                if (item.isFavorite) {containerView.btn_add_to_favorites.text = containerView.context.getString(R.string.favorites_button_deactive)}
-                else {containerView.btn_add_to_favorites.text = containerView.context.getString(R.string.favorites_button_active)}
+                if (item.title.isNullOrBlank()) {
+                    containerView.txt_gif_name.text =
+                        containerView.context.getString(R.string.id_as_name, item?.id)
+                } else containerView.txt_gif_name.text = item.title
+                Glide.with(containerView.context).load(item.url)
+                    .placeholder(R.drawable.ic_placeholder).into(containerView.img_gif_preview)
+                containerView.btn_add_to_favorites.isChecked = item.isFavorite
             }
         }
 
         override fun onClick(view: View?) {
             if (view != null) {
                 when (view.id) {
-                    R.id.btn_add_to_favorites -> this@FeedItemsAdapter.getItem(adapterPosition)?.let {
-                        callback.onAddToFavoritesClick(
-                            it
-                        )
-                    }
+                    R.id.btn_add_to_favorites -> this@FeedItemsAdapter.getItem(adapterPosition)
+                        ?.let {
+                            callback.onAddToFavoritesClick(
+                                it
+                            )
+                        }
                     R.id.img_gif_preview -> this@FeedItemsAdapter.getItem(adapterPosition)?.let {
                         callback.onGifClick(
                             it
